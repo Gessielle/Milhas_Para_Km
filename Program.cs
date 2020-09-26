ea62ace7f2a0948bca54d32d3753b50e8c2824bb
Gessielle <gessielle-barossi@hotmail.com>
2020-09-26 16:27:35 -0300

    Milhas para km

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Milhas_para_Km
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double milhas;
+            double km = 1.609;
+
+            Console.WriteLine("Digite seu valor em milhas: ");
+            milhas = double.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("Seu valor de milhas para Km");
+            Console.WriteLine($"{milhas * km}");
+            Console.Write("\n");
+
+            Console.WriteLine("Pressione Enter para sair!");
+            Console.ReadKey();
+        }
+    }
+}
